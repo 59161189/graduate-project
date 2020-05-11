e625611bec3789d11380e2584e1742a0b541a375
jack <59161189@go.buu.ac.th>
2020-05-11 19:45:04 +0700

    repair pj

---
+++ final graduate project/Assets/Scripts/HPBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    public Slider bar;
+    public Gradient gradient;
+    public Image fill;
+
+    public void start()
+    {
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void hp_decrease(int damage)
+    {
+        bar.value -= damage;
+        fill.color = gradient.Evaluate(bar.normalizedValue);
+    }
+}
